dcd01177f270495d980a36d7eff1c7bfb0a89326
oumaimabenamara <benamaraaoumaymaa98@gmail.com>
2021-05-04 10:54:00 +0000

    add / list / edit event (html + ts)

--- src/app/event-edit/event-edit.component.ts
+++ src/app/event-edit/event-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EventService } from '../services/event.service';
 
 @Component({
   selector: 'app-event-edit',
@@ -7,9 +10,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventEditComponent implements OnInit {
 
-  constructor() { }
+  myID: any;
+  submitted = false;
+  editEventForm: FormGroup = new FormGroup({
+    eventName: new FormControl('', [Validators.required]),
+    // eventPhoto: new FormControl('', [Validators.required]),
+    // eventTags: new FormControl('', [Validators.required]),
+    eventDescription: new FormControl('', [Validators.required]),
+    startDate: new FormControl('', [Validators.required]),
+    endDate: new FormControl('', [Validators.required]),
+    startTime: new FormControl('', [Validators.required]),
+    endTime: new FormControl('', [Validators.required]),
+    location: new FormControl('', [Validators.required]),
+    numberOfTickets: new FormControl('', [Validators.required]),
+    eventType: new FormControl('', [Validators.required]),
+    price: new FormControl('', [Validators.required]),
+  });
+
+  constructor(private router: Router, private eventService : EventService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.myID = this.activatedRoute.snapshot.params['item.id'];
+    this.editEventFunction();
   }
+
+
+  editEventFunction()
+  {
+    this.eventService.getEventById(this.myID).subscribe((response: any)=>
+    {
+      // console.log(response);
+      this.editEventForm.patchValue(response);
+    }, error=>{
+      console.log(error);
+    })
+  }
+
+
+  saveFunction()
+  {
+    this.submitted = true;
+    if (this.editEventForm.invalid)
+    {
+      return;
+    }
+    this.eventService.editEventById(this.myID, this.editEventForm.value).subscribe((response: any)=>{
+      // console.log(response);
+      this.editEventForm.reset();
+      this.submitted= false;
+      this.router.navigate(['event-list']);
+    }, error=>{
+      console.log(error);
+    })
+  }
+  // cancelFunction()
+  // {
+  //   this.editEventForm.reset();
+  //   this.router.navigate(['event-list']);
+  // }
 
 }
--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../services/event.service';
 
 @Component({
   selector: 'app-event-list',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventListComponent implements OnInit {
 
-  constructor() { }
+  eventsArray: any[];
+  constructor(private eventService: EventService) { }
 
   ngOnInit(): void {
+    this.eventService.getAllEvents().subscribe((response: any[] )=>{
+      this.eventsArray = response;
+    }, error=>{
+      console.log(error);
+    })
   }
+
+  deleteEventFunction(id)
+  {
+    this.eventService.deleteEventById(id).subscribe((response: any)=>{
+      this.ngOnInit();
+    }, error=>{
+      console.log(error);
+    })
+  }
+
 
 }
